0a2408b6a07816209da647bf3109579f5cf834be
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-07-26 10:32:26 +0530

    Added solution for Playing With Characters

---
+++ Playing-With-Characters/solution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch;
+    char str[100], str2[100];
+    scanf("%c %s", &ch, &str);
+    scanf("\n");
+    scanf("%[^\n]s", &str2);
+    printf("%c\n", ch);
+    printf("%s\n", str);
+    printf("%s\n", str2);
+    
+  
+    return 0;
+}
